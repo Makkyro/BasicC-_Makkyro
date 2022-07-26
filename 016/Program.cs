bc39b7fc67dbc6b40ae4ebe59d33746525b6e664
Makkyro <70462690+Makkyro@users.noreply.github.com>
2022-07-26 03:16:15 +0300

    Обновление ДЗ 1.6

---
+++ 016/Program.cs
@@ -0,0 +1,9 @@
+﻿//Дано число. 
+//Проверить кратно ли оно 7 и 23
+
+bool b,c;
+int a=Convert.ToInt32(Console.ReadLine());
+b=a%7==0;
+c=a%23==0;
+System.Console.WriteLine(b);
+System.Console.WriteLine(c);
