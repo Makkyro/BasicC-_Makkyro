c435f284ca69ef09fe36e9bbb2695cc8cd18c810
Makkyro <70462690+Makkyro@users.noreply.github.com>
2022-08-07 22:17:05 +0300

    Обновление 2.7

---
+++ 017/Program.cs
@@ -0,0 +1,17 @@
+﻿//Дано число обозначающее день недели. 
+//Выяснить является номер дня недели выходным.
+
+int a=Convert.ToInt32(Console.ReadLine());
+
+if (a==1 || a==2 || a==3 || a==4 || a==5)
+{
+    System.Console.WriteLine("Рабочий день");
+}
+if (a==6 || a==7)
+{
+    System.Console.WriteLine("Выходной день");
+}
+else 
+{
+    System.Console.WriteLine("Нет такого числа дня недели");
+}
