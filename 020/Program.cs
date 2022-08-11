43d90e5a916a4539fbbf50eed2616006a170f435
Makkyro <70462690+Makkyro@users.noreply.github.com>
2022-08-12 00:53:20 +0300

    Обновление 3.0

---
+++ 020/Program.cs
@@ -0,0 +1,28 @@
+﻿//Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+
+System.Console.WriteLine("Ведите число для X = ");
+int X=Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Ведите число для Y = ");
+int Y=Convert.ToInt32(Console.ReadLine());
+
+if (X>0 & Y>0)
+{
+    System.Console.WriteLine("Точка принадлежит первой четверти");
+}
+if (X<0 & Y>0)
+{
+    System.Console.WriteLine("Точка принадлежит второй четверти");
+}
+if (X<0 & Y<0)
+{
+    System.Console.WriteLine("Точка принадлежит третьей четверти");
+}
+if (X>0 & Y<0)
+{
+    System.Console.WriteLine("Точка принадлежит четвертой четверти");
+}
+if (X==0 & Y==0)
+{
+    System.Console.WriteLine("Точка не принадлежит ни одной четверти");
+}
---
+++ 036/Program.cs
@@ -0,0 +1,19 @@
+﻿//Определить, присутствует ли в заданном массиве, некоторое число
+
+int N=12;
+int[] a=new int[N];
+Random random=new Random();
+for(int i=0;i<a.Length;i++)
+    a[i]=random.Next(-9,10);
+    
+int f=400;
+//a[3]=f;
+
+for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],4}");
+
+int j=0;
+while(j<a.Length && a[j]!=f) j++;
+if (j==a.Length) System.Console.WriteLine("\nНе найден");
+else System.Console.WriteLine($"\nЭлемент {f} нашли в {j}");
+
