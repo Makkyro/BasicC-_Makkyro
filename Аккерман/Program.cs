3fd4ac0dff7d78ef99c62cce812458f3010cbb9a
Makkyro <70462690+Makkyro@users.noreply.github.com>
2022-08-20 05:19:50 +0300

    Обновление 5.0

---
+++ Аккерман/Program.cs
@@ -0,0 +1,18 @@
+﻿//Написать программу вычисления функции Аккермана
+
+System.Console.Write("Введите число n: ");
+            int n = System.Convert.ToInt32(Console.ReadLine());
+            System.Console.Write("Введите число m: ");
+            int m = System.Convert.ToInt32(Console.ReadLine());
+            A(n, m);
+            
+            static int A(int n, int m)
+            {
+                if (n == 0) return m + 1;
+                if (n != 0 && m == 0) return A(n - 1, 1);
+                if (n > 0 && m > 0) return A(n - 1, A(n, m - 1));
+                return A(n,m);
+                
+            }
+        
+            System.Console.ReadKey();
